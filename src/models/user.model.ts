efaacfe972e42d69a8fbe43acfaaeda75617fd80
Marvin Ambrose <marvambi@gmail.com>
2023-02-17 07:21:40 +0100

    docs: added user model props deposit and username

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,10 +1,71 @@
 // src/models/user.model.ts
 
-export class User {
-  constructor(
-    public username: string,
-    public password: string,
-    public deposit: number,
-    public role: string
-  ) { }
-}
+import mongoose, { Schema, Model, Document } from 'mongoose';
+
+type UserDocument = Document & {
+  username: string;
+  email: string;
+  password: string;
+  enabled: string;
+  deposit: number;
+  role: string;
+  hash: string;
+  salt: string;
+};
+
+type UserInput = {
+  username: UserDocument['username'];
+  email: UserDocument['email'];
+  password: UserDocument['password'];
+  enabled: UserDocument['enabled'];
+  role: UserDocument['role'];
+  deposit: UserDocument['deposit'];
+  salt: UserDocument['salt'];
+};
+
+const usersSchema = new Schema(
+  {
+    username: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    email: {
+      type: Schema.Types.String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: Schema.Types.String,
+      required: true,
+    },
+    enabled: {
+      type: Schema.Types.Boolean,
+      default: true,
+    },
+    deposit: {
+      type: Schema.Types.Number,
+      default: 0,
+    },
+    role: {
+      type: Schema.Types.ObjectId,
+      ref: 'Role',
+      required: true,
+      index: true,
+    },
+    hash: {
+      type: Schema.Types.String,
+    },
+    salt: {
+      type: Schema.Types.String,
+    },
+  },
+  {
+    collection: 'users',
+    timestamps: true,
+  },
+);
+
+// eslint-disable-next-line max-len
+const User: Model<UserDocument> = mongoose.model<UserDocument>('User', usersSchema);
+
+export { User, UserInput, UserDocument };
