923f66e8b3ca8e6ced79cb3e9e859b6a2545c9d8
Marvin Ambrose <marvambi@gmail.com>
2023-03-03 21:36:21 +0100

    fix: fixed asyncHandler req,res types for updateProduct

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -112,56 +112,126 @@ const getAProd = asyncHandler(async (req: Request, res: Response) => {
 });
 
 // Delete Product
-export const deleteProduct = asyncHandler(async (req: any, res: any) => {
-  const product = await Product.findById(req.params.id);
-  // if product doesnt exist
+export const deleteProduct = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    // Validate that user is a seller
+    const { token } = req.cookies;
+    const { id } = req.params;
 
-  console.log("User: ", req.user);
+    let verified: any;
 
-  if (!product) {
-    res.status(404).send({ message: "Product not found" });
+    const secrets: any = envs.parsed?.JWT_SECRET;
+
+    if (token) {
+      verified = jwt.verify(token, secrets);
+    }
+
+    let sellerId: string;
+    let user: any;
+
+    if (verified?.id !== undefined) {
+      sellerId = verified?.id;
+      user = await User.findOne({ _id: sellerId }).populate("role").exec();
+    }
+
+    if (!user) {
+      return res.status(404).json({ message: `User not found.` });
+    }
+    // Extract role
+    const { _id, role } = user;
+
+    sellerId = _id;
+    //   Validations
+
+    if (role !== "seller") {
+      return res
+        .status(400)
+        .json({ error: "Only sellers can delete products." });
+    }
+
+    const product = await Product.findById(id);
+    // if product doesnt exist
+
+    if (!product) {
+      return res.status(404).send({ message: "Product not found" });
+    }
+    // Match product to its user
+    if (product?.sellerId.toString() !== id) {
+      return res.status(401).send({
+        message: `You're not authorized on this product`,
+      });
+    }
+    await product?.remove();
+    return res.status(200).json({ message: "Product deleted." });
   }
-  // Match product to its user
-  if (product?.sellerId.toString() !== req.user.id) {
-    res.status(401).send({ message: "User not authorized" });
-  }
-  await product?.remove();
-  res.status(200).json({ message: "Product deleted." });
-});
+);
 
 // Update Product
-export const updateProduct = asyncHandler(async (req: any, res: any) => {
-  const { amountAvailable, cost, description, productName } = req.body;
-  const { id } = req.params;
+export const updateProduct = asyncHandler(
+  async (req: Request, res: Response): Promise<any> => {
+    const { amountAvailable, cost, description, productName } = req.body;
+    const { id } = req.params;
+    // Validate that user is a seller
+    const { token } = req.cookies;
 
-  const product = await Product.findById(id);
+    let verified: any;
 
-  // if product doesnt exist
-  if (!product) {
-    res.status(404).send({ message: "Product not found" });
-  }
-  // Match product to its user
-  if (product?.sellerId.toString() !== req.user.id) {
-    res.status(401).send({ message: "User not authorized" });
-  }
+    const secrets: any = envs.parsed?.JWT_SECRET;
 
-  // Update Product
-  const updatedProduct = await Product.findByIdAndUpdate(
-    { _id: id },
-    {
-      productName,
-      amountAvailable,
-      cost,
-      description,
-    },
-    {
-      new: true,
-      runValidators: true,
+    if (token) {
+      verified = jwt.verify(token, secrets);
     }
-  );
+    let sellerId: string;
+    let user: any;
 
-  res.status(200).json(updatedProduct);
-});
+    if (verified?.id !== undefined) {
+      sellerId = verified?.id;
+      user = await User.findOne({ _id: sellerId }).populate("role").exec();
+    }
+
+    // Extract role
+    const { _id, role } = user;
+
+    sellerId = _id;
+    //   Validations
+    if (!user) {
+      return res.status(404).json({ message: `User not found.` });
+    }
+    // Extract role
+    if (role !== "seller") {
+      return res
+        .status(400)
+        .json({ error: "Only sellers can delete products." });
+    }
+    const product = await Product.findById(id);
+
+    // if product doesnt exist
+    if (!product) {
+      res.status(404).send({ message: "Product not found" });
+    }
+    // Match product to its user
+    if (product?.sellerId.toString() !== sellerId) {
+      res.status(401).send({ message: "User not authorized" });
+    }
+
+    // Update Product
+    const updatedProduct = await Product.findByIdAndUpdate(
+      { _id: id },
+      {
+        productName,
+        amountAvailable,
+        cost,
+        description,
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    res.status(200).json(updatedProduct);
+  }
+);
 
 export default {
   createProduct,
